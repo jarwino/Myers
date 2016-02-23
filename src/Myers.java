64a4e15b9e8cbf11b4057c7c9e92613afe4d6468
jarwino <jarwino@gmail.com>
2016-02-23 14:50:58 -0500

    yoooo

---
+++ src/Myers.java
@@ -0,0 +1,31 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+public class Myers {
+
+    public static void main(String[] args) throws AWTException {
+        Robot r = new Robot();
+        for (int i = 0; i < 1000; i++) {
+            r.mouseMove(1330, 390);
+            r.mousePress(InputEvent.BUTTON1_MASK);
+            r.mouseRelease(InputEvent.BUTTON1_MASK);
+            
+            r.delay(100);
+            
+            r.mouseMove(1330, 545);
+            r.mousePress(InputEvent.BUTTON1_MASK);
+            r.mouseRelease(InputEvent.BUTTON1_MASK);
+            
+            r.delay(500);
+            
+            r.mouseMove(1500, 1035);
+            r.mousePress(InputEvent.BUTTON1_MASK);
+            r.mouseRelease(InputEvent.BUTTON1_MASK);
+        }
+        
+        
+        
+    }
+
+}
